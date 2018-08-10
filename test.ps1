d5e71304be6657c509cc7da069dea699a93416c3
Napster653 <100330656@alumnos.uc3m.es>
2018-08-10 19:27:48 +0200

    Color coded output

--- test.ps1
+++ test.ps1
@@ -1,14 +1,12 @@
 ﻿1..255 | foreach { 
     $IP = "192.168.1.$_"
 
-    Write-Host -NoNewline 'Testing ' $IP'...'
-
     If (Test-Connection $IP -count 1 -Quiet)
     {
-        Write-Host 'OK'
+        Write-Host $IP -ForegroundColor Green
     }
     Else
     {
-        Write-Host 'ERROR'
+        Write-Host $IP -ForegroundColor Red
     }
 }
