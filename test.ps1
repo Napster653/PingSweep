4f90d19bc0ce27a5699c2ef4b0f27ebb86a20535
Napster653 <100330656@alumnos.uc3m.es>
2018-08-10 18:42:24 +0200

    initial commit

---
+++ test.ps1
@@ -0,0 +1,14 @@
+﻿1..255 | foreach { 
+    $IP = "192.168.1.$_"
+
+    Write-Host -NoNewline 'Testing ' $IP'...'
+
+    If (Test-Connection $IP -count 1 -Quiet)
+    {
+        Write-Host 'OK'
+    }
+    Else
+    {
+        Write-Host 'ERROR'
+    }
+}
